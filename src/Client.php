f1a1e8349dcda490bedeccbc3491e45ff38c3fb2
Kris <kris@meridian-ds.com>
2014-12-23 16:59:02 -0600

    refactored the Client object to be more properly dependency injected and updated to using guzzle 5.1.x

--- src/Client.php
+++ src/Client.php
@@ -11,76 +11,58 @@ use GuzzleHttp\Client as GuzzleClient;
 
 class Client {
 
-  use RequestTrait;
-
   const BASE_URL         = 'https://cloudapi.acquia.com/{version}/';
   const BASE_PATH        = 'v1';
 
-  protected $sitesClass = 'Acquia\Cloud\Api\SDK\Sites';
+  /**
+   * @var ObjectFactoryInterface
+   */
+  protected $factory;
 
-  protected $objectFactory;
-
-  protected $sites;
-
-  public function __construct($user, $pass) {
-    $this->createGuzzleClient($user, $pass);
+  /**
+   * @param ObjectFactoryInterface $factory
+   */
+  public function __construct(ObjectFactoryInterface $factory) {
+    $this->factory = $factory;
   }
 
-  protected function createGuzzleClient($user, $pass) {
+  /**
+   * @param $user
+   * @param $pass
+   * @return static
+   */
+  public static function create($user, $pass) {
+    $factory = static::createFactory($user, $pass);
+    return new static($factory);
+  }
+
+  /**
+   * @param $user
+   * @param $pass
+   * @return ObjectFactory
+   */
+  public static function createFactory($user, $pass) {
     $config = [
-      'base_url' => [$this::BASE_URL, ['version' => $this::BASE_PATH]],
+      'base_url' => [static::BASE_URL, ['version' => static::BASE_PATH]],
       'defaults' => [
         'auth'    => [$user, $pass],
       ],
     ];
-    $this->client(new GuzzleClient($config));
-  }
-
-  /**
-   * Instantiates an ObjectFactory if one doesn't exist and returns the object.
-   *
-   * @return ObjectFactoryInterface
-   */
-  protected function objectFactory() {
-    if (!isset($this->objectFactory)) {
-      $this->objectFactory = new ObjectFactory($this->client());
-    }
-    return $this->objectFactory;
-  }
-
-  /**
-   * Returns the current ObjectFactory.
-   *
-   * @return ObjectFactoryInterface
-   */
-  public function getObjectFactory() {
-    return $this->objectFactory();
-  }
-
-  /**
-   * Sets a new ObjectFactory.
-   *
-   * @param ObjectFactoryInterface $factory
-   *
-   * @return $this
-   */
-  public function setObjectFactory(ObjectFactoryInterface $factory) {
-    $this->objectFactory = $factory;
-    return $this;
+    return new ObjectFactory(new GuzzleClient($config));
   }
 
   /**
    * @return array|mixed
    */
   public function getAliases() {
-    return $this->objectFactory()->getAliases();
+    return $this->factory->getAliases();
   }
 
   /**
    * @return \Acquia\Cloud\Api\SDK\Sites|\Acquia\Cloud\Api\SDK\SiteInterface[]
    */
   public function getSites() {
-    return $this->objectFactory()->getSites();
+    return $this->factory->getSites();
   }
 
 }
